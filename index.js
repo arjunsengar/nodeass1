386b65aef6f9ed769f5b8248f276125ea898d4ee
Arjun Singh <arjunsengar29@gmail.com>
2022-05-23 19:09:14 +0530

    first commit

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const http =require('http');
 //require() is built in function to include function it is very important  to creating server
 
 
-http.createServer((req,res)=>{   //here creating a server this is Es5 version
+http.createServer((req,res)=>{   // creating a server
 
- res.write("HELLO WORLD");  //here sending response backend to frontend here also send any type like json and html file
+ res.write("HELLO WORLD");  //sending response backend to frontend and we also send any type like json & html file
 
-res.write("<h1>Welcome To Pune</h1>") //send data in html formate
+res.write("<h1>Welcome To Pune</h1>") //passing in html formate
 
-res.write(JSON.stringify({name:"arjun",age:25}))  //send data  in json formate
+res.write(JSON.stringify({name:"arjun",age:25}))  //pass data in json formate
 
-res.end();  //this is require  to end the server
-}).listen(8080);  //and this is port number where the code run meance type in url 8000 then run the code
+res.end();  
+}).listen(8080);  //this a is port number(8080) where we run the code 
 
-
-//any change in this code then require to restart the server
